1a08343aae11557e9b7804bac3349374dcd11fd4
Marc Gueury <marc.gueury@oracle.com>
2025-11-02 18:30:31 +0100

    v2

--- app_openai_gtw/start.sh
+++ app_openai_gtw/start.sh
@@ -6,8 +6,9 @@ curl -s -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/insta
 export TF_VAR_compartment_ocid=`cat /tmp/instance.json | jq -r .compartmentId`
 export TF_VAR_region=`cat /tmp/instance.json | jq -r .region`
 
+cd OCI_GenAI_access_gateway
 source myenv/bin/activate
-cd OCI_GenAI_access_gateway/app
+cd app
 # python app.py
 gunicorn app:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --timeout 600 --bind 0.0.0.0:8088 2>&1 | tee ../../app_openapi_gtw.log
 
